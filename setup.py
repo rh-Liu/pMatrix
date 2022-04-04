be779e3a082d62a4358346ad521de66af816908f
algo21-221040051 <100055922+algo21-221040051@users.noreply.github.com>
2022-04-04 18:23:26 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     author_email='1398151992@qq.com',
     url='https://github.com/algo21-221040051/pMatrix',
     # packages=find_packages(),
-    # package_dir={'': 'pMatrix'},
+    package_dir={'': 'pMatrix'},
     packages=['pMatrix'],
 )
